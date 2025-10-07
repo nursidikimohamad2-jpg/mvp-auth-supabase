a6513ce0d5e9382c5be38aa03cb79396a26fbca6
nursidikimohamad2-jpg <nursidikimohamad2@gmail.com>
2025-10-07 19:46:47 +0800

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,1 @@
-// Inisialisasi koneksi Supabase (pastikan hanya ada file ini satu kali di seluruh project)
 
-const SUPABASE_URL = "https://vkhfrkbonzzydwcmrcka.supabase.co"; // ✅ URL sudah benar
-const SUPABASE_ANON_KEY = 
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZraGZya2Jvbnp6eWR3Y21yY2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4Mjg4MjksImV4cCI6MjA3NTQwNDgyOX0.NHV21GAv9cNBgxMAiZ8gs21v7hLLhLTCL3tTBWqbrXE";
-
-window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
