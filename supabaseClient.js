f61e736a881f08a1bf2d1fbe46ed5c8fc9f041e2
nursidikimohamad2-jpg <nursidikimohamad2@gmail.com>
2025-10-07 19:13:37 +0800

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
-// Inisialisasi koneksi Supabase
+// Inisialisasi koneksi Supabase (pastikan hanya ada file ini sekali saja)
 const SUPABASE_URL = "https://vkhrfrkbonzzydwcmrcka.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZraGZya2Jvbnp6eWR3Y21yY2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4Mjg4MjksImV4cCI6MjA3NTQwNDgyOX0.NHV21GAv9cNBgxMAiZ8gs21v7hLLhLTCL3tTBWqbrXE";
 
