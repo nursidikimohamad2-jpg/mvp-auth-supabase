a7f4af5a9a48c7ecff138599cabce6b33bba3521
nursidikimohamad2-jpg <nursidikimohamad2@gmail.com>
2025-10-07 18:15:10 +0800

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,3 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
 const SUPABASE_URL = "https://vkhfrfkbonzzydwcmrcka.supabase.co";
-const SUPABASE_ANON_KEY = "PASTE_ANNON_KEY_KAMU_DI_SINI";
+const SUPABASE_ANON_KEY = "PASTE_ANON_PUBLIC_KEY_DARI_SUPABASE";
+
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+</script>
