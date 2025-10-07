d7a3bbf21856565a74eb1799467f72ff38a0d3c6
nursidikimohamad2-jpg <nursidikimohamad2@gmail.com>
2025-10-07 19:03:32 +0800

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
-// Muat koneksi Supabase
-const SUPABASE_URL = "https://vkhfrfkbonzzydwcmrcka.supabase.co";
-const SUPABASE_ANON_KEY = "PASTE_ANON_PUBLIC_KEY_DARI_SUPABASE";
+// Inisialisasi koneksi ke Supabase
+const SUPABASE_URL = "https://vkhrfrkbonzzydwcmrcka.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZraGZya2Jvbnp6eWR3Y21yY2thIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4Mjg4MjksImV4cCI6MjA3NTQwNDgyOX0.NHV21GAv9cNBgxMAiZ8gs21v7hLLhLTCL3tTBWqbrXE";
 
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
